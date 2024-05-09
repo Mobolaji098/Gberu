dff6baeae27b9ad4361801ec67b58a0a4c939046
Barterway <barterwayapp.gmail.com>
2024-05-09 16:04:10 +0100

    edits

--- src/components/Pages/Page1.tsx
+++ src/components/Pages/Page1.tsx
@@ -6,7 +6,7 @@ export default function Page1() {
 
   return (
     <div className="bg-white dark:bg-black ">
-    <div className={`flex flex-col px-12 relative lg:max-w-[1300px] max-w-[750px] w-full m-auto lg:mx-20 mt-10 min-h-screen  `}>
+    <div className={`flex flex-col px-12 relative lg:max-w-[1300px] max-w-[750px] w-full m-auto lg:mx-20 mt-10 max-h-screen lg:mb-32  `}>
       <div className=" flex flex-col  lg:w-1/2 xl:w-3/5 lg:pt-0 text-center lg:text-left lg:items-start items-center w-full justify-center">
         <h1 className={`text-gray-800 dark:text-white lg:text-5xl text-3xl lg:mt-28 mt-12  lg:tracking-wider `}>
           <span className="italic font-medium">Request </span>
@@ -26,18 +26,14 @@ export default function Page1() {
         </ButtonColored>
       </div>
       <div className="flex justify-center items-center pt-10">
-      <Image
-        className=" lg:absolute lg:top-10 lg:right-40 lg:ml-8 lg:translate-x-0 "
+      <img
+        className=" lg:absolute lg:top-10 lg:right-40 lg:ml-8 lg:translate-x-0 lg:w-[450px] lg:h-[450px] w-[300px] h-[300px]"
         src="/gberuPersonCarrybox.png"
-        width={450}
-        height={450}
         alt="Logistics picture"
       />
-      <Image
-        className=" z-10 absolute top-[65px] right-0 -translate-x-[-70px] lg:block md:hidden max-md:hidden"
+      <img
+        className=" z-10 absolute top-[65px] right-0 -translate-x-[-70px] lg:block hidden w-[873px] h-[873px]"
         src="/gberuBus.png"
-        width={873}
-        height={873}
         alt="Logistics picture"
       />
       </div>
